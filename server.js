e6052d6754e6b6512a56a140a1abe224954a783a
Ilya <geil1997@mail.ru>
2025-02-14 16:07:24 +0300

    new test method

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("success");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.get("/test", (req, res) => {
+  res.send("success");
+});
+
+// Запуск сервера
+app.listen(PORT, () => {
+  const HOST = process.env.HOST || "localhost";
+  console.log(`Сервер запущен по адресу ${baseUrl}`);
 });
