6abf261fb87727ce10ea06c0c85d1c0f4518aa73
Ilya <geil1997@mail.ru>
2025-02-17 20:50:19 +0300

    db info update

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize("bd-geula", "root", "Fud5e@$$", {
+module.exports = new Sequelize("db_geula", "root", "Fud5e@$$", {
   host: "79.174.88.83",
   port: "15997",
   dialect: "postgres",
