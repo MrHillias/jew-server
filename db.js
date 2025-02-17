71c923cc583031ee7dd3455dc629c728aed57cc4
Ilya <geil1997@mail.ru>
2025-02-17 20:48:56 +0300

    db reset

--- db.js
+++ db.js
@@ -2,6 +2,6 @@ const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize("bd-geula", "root", "Fud5e@$$", {
   host: "79.174.88.83",
-  port: "18631",
+  port: "15997",
   dialect: "postgres",
 });
