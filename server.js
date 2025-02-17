07c32db215258470867c316bf0b73d2608bb492c
Ilya <geil1997@mail.ru>
2025-02-17 18:55:49 +0300

    HOST update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 // Используйте переменную HOST
-const HOST = process.env.HOST || "localhost";
+const HOST = process.env.HOST || "0.0.0.0";
 
 app.get("/", (req, res) => {
   res.send("success");
