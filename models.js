d6616979e6721526c844e421a118569fa03d4fee
Ilya <geil1997@mail.ru>
2025-02-23 23:20:03 +0300

    bug fix table name

--- models.js
+++ models.js
@@ -1,41 +1,47 @@
 const sequelize = require("./db");
 const { Sequelize, DataTypes } = require("sequelize");
 
-const User = sequelize.define("User", {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  firstName: { type: DataTypes.STRING, allowNull: true },
-  lastName: { type: DataTypes.STRING, allowNull: true },
-  fatherName: { type: DataTypes.STRING, allowNull: true },
-  birthDate: { type: DataTypes.DATE, allowNull: true },
-  mobileNumber: { type: DataTypes.INTEGER, allowNull: true, defaultValue: 0 },
-  email: { type: DataTypes.STRING, allowNull: true },
-  gender: { type: DataTypes.STRING, allowNull: true },
-  address: {
-    type: DataTypes.JSON,
-    allowNull: true,
-    defaultValue: {
-      city: null,
-      street: null,
-      houseNumber: null,
-      building: null,
-      apartment: null,
-      metroStation: null,
+const User = sequelize.define(
+  "User",
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    firstName: { type: DataTypes.STRING, allowNull: true },
+    lastName: { type: DataTypes.STRING, allowNull: true },
+    fatherName: { type: DataTypes.STRING, allowNull: true },
+    birthDate: { type: DataTypes.DATE, allowNull: true },
+    mobileNumber: { type: DataTypes.INTEGER, allowNull: true, defaultValue: 0 },
+    email: { type: DataTypes.STRING, allowNull: true },
+    gender: { type: DataTypes.STRING, allowNull: true },
+    address: {
+      type: DataTypes.JSON,
+      allowNull: true,
+      defaultValue: {
+        city: null,
+        street: null,
+        houseNumber: null,
+        building: null,
+        apartment: null,
+        metroStation: null,
+      },
+    },
+    religiousInfo: {
+      type: DataTypes.JSON,
+      allowNull: true,
+      defaultValue: {
+        keepsSabbath: false,
+        keepsKosher: false,
+        hasTT: false,
+        seminarParticipant: false,
+        hasCommunityBooks: false,
+        childrenCamp: false,
+        passover: false,
+        isInNeed: false,
+      },
     },
   },
-  religiousInfo: {
-    type: DataTypes.JSON,
-    allowNull: true,
-    defaultValue: {
-      keepsSabbath: false,
-      keepsKosher: false,
-      hasTT: false,
-      seminarParticipant: false,
-      hasCommunityBooks: false,
-      childrenCamp: false,
-      passover: false,
-      isInNeed: false,
-    },
-  },
-});
+  {
+    tableName: "users", // Явное указание имени таблицы
+  }
+);
 
 module.exports = User;
