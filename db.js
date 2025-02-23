44bda662ef5f74d89b7b5eaa0019fb90679ae3d7
Ilya <geil1997@mail.ru>
2025-02-23 23:27:20 +0300

    sql logging added

--- db.js
+++ db.js
@@ -4,4 +4,5 @@ module.exports = new Sequelize("db_geula", "root", "Fud5e@$$", {
   host: "79.174.88.83",
   port: "15997",
   dialect: "postgres",
+  logging: console.log, // Включаем логирование SQL-запросов
 });
