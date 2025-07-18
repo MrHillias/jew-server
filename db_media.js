b1916a3148d7b559f60334627c7a3cb6bb174165
Ilya <geil1997@mail.ru>
2025-07-19 00:07:58 +0300

    updated hosts

--- db_media.js
+++ db_media.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize("db_geula", "root", "Fud5e@$$", {
-  host: "79.174.88.83",
-  port: "15997",
+  host: "79.174.88.183",
+  port: "19894",
   dialect: "postgres",
   logging: console.log, // Включаем логирование SQL-запросов
 });
